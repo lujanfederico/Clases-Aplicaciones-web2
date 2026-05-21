9fd8fe8dec72198f6d604c03e5549b4ed2c4984a
lujanfederico <fedelujan2016@gmail.com>
2026-05-21 14:38:11 -0300

    docker / database

---
+++ clase-20260520-PG - 2/modulos/productos/vista.productos.mjs
@@ -0,0 +1,15 @@
+// Reestructurar la vista
+
+export function obtenerTodos(datosCompletos) {
+    // 
+    return datosCompletos }
+
+export function obtenerUno(datosCompletos) {
+    // 
+    return datosCompletos
+}
+
+export function eliminarUno(datosCompletos) {
+    // 
+    return datosCompletos
+}
